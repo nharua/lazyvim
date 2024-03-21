aad6f5da1b24c20271cfe0869d145a08974eac1e
Huynh Van Vinh <vinhhv@live.com>
2024-03-21 15:48:30 +0700

    Added treesitter

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
+-- TODO: The very first thing you should do is to run the command `:checkhealth` in Neovim.
+--
+--     If you don't know what this means, type the following:
+--       - <escape key>
+--       - :
+--       - checkhealth
+--       - <enter key>
+--
+--     Read neovim guileline, run the command `:Tudor
+--
+--     MOST IMPORTANTLY, we provide a keymap "<space>sh" to [s]earch the [h]elp documentation,
+--       which is very useful when you're not sure exactly what you're looking for.
+
 -- Load options file 
 require ("options")
 -- Load keymap
