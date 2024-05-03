da4e296007e7bef28d9e00ce25409ec31a77a66b
Huynh Van Vinh <vinhhv@live.com>
2024-05-03 13:12:07 +0700

    Update gitsings and added vim fugitive

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,13 +1,22 @@
--- See `:help gitsigns` to understand what the configuration keys do
-return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-	"lewis6991/gitsigns.nvim",
-	opts = {
-		signs = {
-			add = { text = "+" },
-			change = { text = "~" },
-			delete = { text = "_" },
-			topdelete = { text = "‾" },
-			changedelete = { text = "~" },
+return {
+	{
+		"tpope/vim-fugitive",
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
 		},
+		config = function()
+			require("gitsigns").setup()
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame <CR>", {})
+		end,
 	},
 }
